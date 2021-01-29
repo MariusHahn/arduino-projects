0ef4c153ee13bec084b5ddd4adae5fe378834ace
Marius Hahn <marius.hahn@live.com>
2021-01-29 23:50:09 +0100

    color letters when code is emitted

--- 433MhzEmitter/remote/js/main.js
+++ 433MhzEmitter/remote/js/main.js
@@ -6,11 +6,49 @@ window.onload = () => {
     }
 }
 
-const responseDiv = document.getElementById("response_div");
-const responseIframe = document.getElementById("response");
-responseIframe.onload = function() {
-    const responseText = responseIframe.contentWindow.document.body.innerHTML;
-    const displayContent = responseText.split(":")[1].trim()
-    console.log(`response: ${displayContent}`)
-    responseDiv.innerText = displayContent;
+function getResponseIframe() {
+    return document.getElementById("response")
+}
+
+getResponseIframe().onload = colorText;
+
+function getEmittedCode() {
+    return getResponseIframe()
+    .contentWindow
+    .document
+    .body
+    .innerHTML
+    .split(":")[1].trim()
+}
+
+function getElementIdAndColor(code) {
+    const green = "#00ff00"
+    const red = "#ff0000"
+    switch(code) {
+        case "2221916":
+            return {ids: ["a", "c", "d"], color: green}
+        case "2416012":
+            return {ids: ["a", "c", "d"], color: red}
+        case "2126053":
+            return {ids: ["b"], color: green}
+        case "2263253":
+            return {ids: ["b"], color: red}
+        case "2416014":
+            return {ids: ["c"], color: green}
+        case "2309998":
+            return {ids: ["c", "a"], color: red}
+        case "2263255":
+            return {ids: ["d"], color: green}
+        case "2126055":
+            return {ids: ["d", "a"], color: red}
+    }
+}
+
+function colorText() {
+    const emittedCode = getEmittedCode()
+    const colorAndCode = getElementIdAndColor(emittedCode);
+    console.log(colorAndCode)
+    colorAndCode.ids
+        .map(id => document.getElementById(id))
+        .forEach(el => el.style.color = colorAndCode.color);
 }
